c9ec2798928c9ee18459e942bfd9b6215c3a2001
lewelyn7 <progit8itakademia>
2020-03-08 14:43:33 +0100

    12.5%

--- HamielecKarol/cw01/zad1/zad1.c
+++ HamielecKarol/cw01/zad1/zad1.c
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 
 #define FILE_LENGTH 25
+#define COMMAND_SIZE 256
 
     struct main_block_arr{
         int size;
+        int idx;
         struct edit_block_arr *arr;
     };
 
@@ -18,65 +20,121 @@
 
     struct main_block_arr create_main_arr(int size){
         struct  main_block_arr main_arr;
-        main_arr.size = 0;
+        main_arr.size = size;
+        main_arr.idx = -1;
+        main_arr.arr = (struct edit_block_arr*) calloc(size, sizeof(struct edit_block_arr));
         return main_arr;
     }
 
-    struct main_block_arr define_files_seq(char* files_seq, struct main_block_arr main_arr){
-        
-        while(files_seq != NULL){
-            char * filea = calloc(FILE_LENGTH, sizeof(filea));
-            char * fileb = calloc(FILE_LENGTH, sizeof(fileb));
-            main_arr.arr = (*edit_block_arr) calloc(1, sizeof(edit_block_arr));
+    struct main_block_arr define_files_seq(char** files_seq, struct main_block_arr main_arr){
+        for(int i = 0; i < main_arr.size; i++){
+            main_arr.idx++;
 
-
-            char * start_pointer = files_seq;
-            if(start_pointer == NULL){
-                return main_arr;
-            }
-            char * space_pointer = strchr(start_pointer + 1, ' ');
-            if(space_pointer == NULL){
-                return main_arr;
-            }
-            char * end_pointer = strchr(space_pointer+1, ' ');
-            if(end_pointer == NULL){
-                end_pointer = strchr(space_pointer+1, '\0');
-            }
-
-
-            int i = 0;
-            while(start_pointer != space_pointer){
-                filea[i] = *start_pointer;
-                start_pointer++;
-                i++;
-            }
-            filea[i] = '\0';
-
-            i = 0;
-            space_pointer++;
-            while(space_pointer != end_pointer){
-
-                fileb[i] = *space_pointer;
-                i++;
-                space_pointer++;
-            }
-            printf("a: %s \r\n", filea);
-            printf("b: %s \r\n", fileb);
+            main_arr.arr[i].size = 0;
+            main_arr.arr[i].file1 = (char*) calloc(FILE_LENGTH, sizeof(char));
+            main_arr.arr[i].file2 = (char*) calloc(FILE_LENGTH, sizeof(char));
+            strcpy(main_arr.arr[i].file1, files_seq[i*2]);
+            strcpy(main_arr.arr[i].file2, files_seq[i*2+1]);
             
-
-
-
-            if(*end_pointer == '\0'){
-                files_seq = NULL;
-            }else{
-                files_seq = end_pointer + 1;
-            }
         }
         return main_arr;
+
     }
 
-int main(void ){
-    struct main_block_arr  starr= create_main_arr();
-    define_files_seq("a.txt b.txt c.txt d.txt", starr);
+    struct main_block_arr compare(struct main_block_arr starr){
+        char * command = calloc(COMMAND_SIZE, sizeof(char));
+        char str_number[5];
+        for(int i = 0; i <= starr.idx; i++){
+            for(int i = 0; i < COMMAND_SIZE; i++) command[i] = 0;
+            strcpy(command, "diff ");
+            strcat(command, starr.arr[i].file1);
+            strcat(command, " ");
+            strcat(command, starr.arr[i].file2);
+            strcat(command, " > ");
+            strcat(command, "diff_op");
+            sprintf(str_number, "%d", i);        
+            strcat(command, str_number);        
+            strcat(command, ".tmp");        
+            printf("command: %s", command);
+            system(command);
+        }
+        return starr;
+    }
+    void print_files(struct main_block_arr main){
+        for(int i = 0; i <= main.idx; i++){
+            printf("file: %s : %s \r\n", main.arr[i].file1, main.arr[i].file2);
+        }
+    }
+    int count_edit_blocks(FILE * f){
 
+        int op_counter = 1;
+        char ch1, ch2;
+        fseek (f, 0, SEEK_SET);
+        ch1 = fgetc(f);
+        while( !feof(f)) {
+            ch2 = fgetc(f);
+            printf("%c", ch1);
+            if(ch1 == '\n' && ch2 > 47 && ch2 < 58){
+                op_counter++;
+            }
+            ch1 = ch2;
+        }
+        fseek (f, 0, SEEK_SET);
+        return op_counter;
+    }
+    struct main_block_arr fill_with_data(struct main_block_arr starr){
+    
+        for(int i = 0; i <= starr.idx; i++){
+            char *fname = (char*) calloc(FILE_LENGTH, sizeof(char));
+            char str_number[5];        
+            strcat(fname, "diff_op");
+            sprintf(str_number, "%d", i);        
+            strcat(fname, str_number);        
+            strcat(fname, ".tmp"); 
+
+            FILE *f = fopen(fname, "r");
+            if (f == NULL)
+            {
+                perror("Cannot open file");
+                exit(1);
+            }
+            fseek (f, 0, SEEK_END);
+            int  length = ftell (f);
+            fseek (f, 0, SEEK_SET);
+            int op_counter = count_edit_blocks(f);
+            starr.arr[i].size = op_counter;
+            printf("op_cnt: %d", op_counter);
+            starr.arr[i].edit_ops = (char**) calloc(op_counter, sizeof(char*));
+
+            for(int j = 0; j < op_counter; j++){
+                starr.arr[i].edit_ops[j] = (char*) calloc(length, sizeof(char));
+                char ch1, ch2;
+                ch1 = fgetc(f);
+                int end_of_block = 0;
+                int ii = 0;
+                while(!end_of_block && !feof(f)) {
+                    ch2 = fgetc(f);
+                    if(ch1 == '\n' && ch2 > 47 && ch2 < 58){
+
+                        end_of_block = 1;
+                    }
+                    starr.arr[i].edit_ops[j][ii] = ch1;
+                    ii++;
+                    ch1 = ch2;
+                }
+                fseek(f, -1, SEEK_CUR);
+            }
+
+
+        }
+        return starr;
+    }
+
+int main(int argc, char** argv){
+    struct main_block_arr starr = create_main_arr(argc/2);
+    starr = define_files_seq(argv + 1, starr);
+    print_files(starr);
+    compare(starr);
+    fill_with_data(starr);
+    
 }

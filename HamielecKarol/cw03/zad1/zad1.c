6c6a5c40c35e00bc9584ca8710b9411066d093af
lewelyn7 <progit8itakademia>
2020-03-21 16:33:34 +0100

    15% tylko

---
+++ HamielecKarol/cw03/zad1/zad1.c
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/times.h>
+#include <time.h>
+#include <dirent.h>
+#include <sys/wait.h>
+
+struct config{
+    int mtime_on;
+    int mtime;
+    int atime_on;
+    int atime;
+    int maxdepth;
+    int maxdepth_on;
+    char start_point[512];
+};
+
+int match(time_t xtime, int arg){
+    time_t now = time(NULL);
+    time_t diff = difftime(now, xtime);
+    int days = diff/3600/24;
+    if(arg < 0){
+        arg *= -1;
+        if(days <= arg){
+            return 1;
+        }else{
+            return 0;
+        }
+    }else if(arg == 0){
+        if(days == 0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }else{
+        if(days >= arg){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
+
+
+void find(struct config cfg){
+    int child_pid = fork();
+    if(child_pid == 0){
+        printf("PID: %d dir: %s\r\n", (int)getpid(), cfg.start_point);
+        execlp("ls", "-l", NULL);
+    }
+    wait(NULL);
+    DIR* direk = opendir(cfg.start_point);
+    if(direk == NULL){
+        printf("blad otwarcia kataogu");
+        exit(-1);
+    }
+
+    struct dirent* curr_rec;
+    struct stat buf_stat;
+    char pathfull[256];
+    
+
+
+
+    curr_rec = readdir(direk);
+    while(curr_rec != NULL){
+        strcpy(pathfull, cfg.start_point);
+        strcat(pathfull, (curr_rec->d_name));
+        if(lstat(pathfull, &buf_stat) != 0){
+            printf("blad czytania pliku");
+            exit(-1);
+        }
+        if(cfg.atime_on && !match(buf_stat.st_atime, cfg.atime)){
+            curr_rec = readdir(direk);           
+            continue;
+
+        }
+        if(cfg.mtime_on && !match(buf_stat.st_mtime, cfg.mtime)){
+            curr_rec = readdir(direk);
+            continue;
+
+        }
+        // printf("%s %d", pathfull, (int)buf_stat.st_nlink);
+        
+        // if(S_ISLNK(buf_stat.st_mode)){
+        //     printf(" slink ");           
+        // }else if(S_ISDIR(buf_stat.st_mode)){
+        //     printf(" dir ");
+        // }else if(S_ISCHR(buf_stat.st_mode)){
+        //     printf(" char dev ");
+        // }else if(S_ISBLK(buf_stat.st_mode)){
+        //     printf(" block dev ");
+        // }else if(S_ISREG(buf_stat.st_mode)){
+        //     printf(" file ");           
+        // }else if(S_ISFIFO(buf_stat.st_mode)){
+        //     printf(" fifo ");           
+        // }else if(S_ISSOCK(buf_stat.st_mode)){
+        //     printf(" sock ");           
+        // }
+        // printf(" %d ", (int)buf_stat.st_size);
+
+        // ts = localtime(&buf_stat.st_atime);
+        // strftime(buff, sizeof(buff), " %Y-%m-%d %H:%M:%S ", ts);
+        // printf(" %s ", buff);
+
+        // ts = localtime(&buf_stat.st_mtime);
+        // strftime(buff, sizeof(buff), " %Y-%m-%d %H:%M:%S ", ts);
+        // printf(" %s ", buff);
+
+        // printf("\r\n");
+        if(S_ISDIR(buf_stat.st_mode) && !S_ISLNK(buf_stat.st_mode) && strcmp(curr_rec->d_name, ".") && strcmp(curr_rec->d_name, "..")){
+            if( !cfg.maxdepth_on  || (cfg.maxdepth_on && cfg.maxdepth > 0)){
+
+                
+                struct config cfg2 = cfg;
+                cfg2.maxdepth--;
+                strcpy(cfg2.start_point, pathfull);
+                strcat(cfg2.start_point, "/");
+                find(cfg2);
+            }
+        }
+        curr_rec = readdir(direk);
+    }
+    closedir(direk);
+}
+
+int main(int argc, char** argv){
+    struct config cfg;
+    cfg.atime_on = 0;
+    cfg.mtime_on = 0;
+    cfg.maxdepth_on = 0;
+
+    for(int i = 1; i < argc; i++){
+        
+        if(!strcmp(argv[i], "-mtime")){
+            cfg.mtime_on = 1;
+            if(i == argc-1){
+                printf("blad argumentow");
+                exit(-1);
+            }
+            i++;
+            sscanf(argv[i], "%d", &cfg.mtime);
+
+        }else if(!strcmp(argv[i], "-atime")){
+            cfg.atime_on = 1;
+            if(i == argc-1){
+                printf("blad argumentow");
+                exit(-1);
+            }
+            i++;
+            sscanf(argv[i], "%d", &cfg.atime);
+            
+        }else if(!strcmp(argv[i], "-maxdepth")){
+            cfg.maxdepth_on = 1;
+            if(i == argc-1){
+                printf("blad argumentow");
+                exit(-1);
+            }
+            i++;
+            sscanf(argv[i], "%d", &cfg.maxdepth);
+            
+        }else{
+
+            // if(argv[i][0] == '/'){
+            //     strcpy(cfg.start_point, argv[i]);
+            // }else{
+            //     getcwd(cfg.start_point, 512);
+            //     strcat(cfg.start_point, "/");
+            //     strcat(cfg.start_point, argv[i]);
+            // }
+            strcpy(cfg.start_point, argv[i]);
+            strcat(cfg.start_point, "/");
+
+           
+        
+        }
+    }
+    find(cfg);
+}
